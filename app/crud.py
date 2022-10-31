e374cf7f1b951d5d57fe1d59145f9d676dd01d08
ruthnohe23@gmail.com <ruthnohe23@gmail.com>
2022-10-31 02:05:16 -0300

    act_backup_informes

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,12 @@
 from statistics import mode
+from unittest import result
 from fastapi import APIRouter,Depends
 from datetime import datetime, timezone
 from db.database import get_db
 from schemas import *
 from sqlalchemy.orm import Session
+from sqlalchemy import text
+from db.database import SessionLocal,engine,get_db,Base
 
 from db import models
 
@@ -18,7 +21,34 @@ def get_departments(db: Session):
     return db.query(models.departments).all()
 
 def get_informe1(db: Session):
-    return db.query(models.hiredemployees).all()
+    sql= text('''SELECT department,job,sum(case when mes in (1,2,3) then 1 else 0 end) AS "Q1",
+                sum(case when mes in (4,5,6) then 1 else 0 end) AS "Q2",
+                sum(case when mes in (7,8,9) then 1 else 0 end) AS "Q3",
+                sum(case when mes in (10,11,12) then 1 else 0 end) AS "Q4"
+                FROM
+                    (select A.id as emplo_id,B.department as department,C.job as job,A.datetime, 
+                        cast(substring(A.datetime,6,2) as integer) as mes
+                    from hired_employees as A 
+                    join departments B on cast(A.department_id as int)=cast(B.id as int)
+                    join jobs C on A.job_id=C.id
+                    WHERE substring(A.datetime,1,4)='2021') TT 
+                GROUP BY department,job
+                ORDER BY department,job asc''')
+    results = engine.execute(sql).fetchall()
+    return results
 
 def get_informe2(db: Session):
-    return db.query(models.hiredemployees).all()
+    sql2= text('''SELECT A.department_id as id,B.department,count(*) AS hired
+                FROM hired_employees A
+                join departments B on cast(A.department_id as int)=cast(B.id as int)
+                WHERE substring(A.datetime,1,4)='2021'
+                GROUP BY department_id,department
+                HAVING count(*)>
+	                (SELECT avg(TOTAL) as media 
+	                FROM (select COUNT(department_id) as TOTAL from hired_employees A
+	                join departments B on cast(A.department_id as int)=cast(B.id as int)
+	                where substring(A.datetime,1,4)='2021' group by department_id) AA)
+                order by count(*) desc''')
+
+    results2 = engine.execute(sql2).fetchall()
+    return results2
--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ from typing import Optional,List
 from datetime import datetime
 from fastapi import Depends,Request
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from schemas import *
 from db.models import hiredemployees,departments,jobs
 from db.database import SessionLocal,engine,get_db,Base
 import crud
+import backup
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -30,14 +32,29 @@ def obtener_jobs(request: Request,db: Session = Depends(get_db)):
   return jobss
 
 @app.get("/challenge2/informe1")
-def informe1():
-  print("req2")
-  return True
+def informe1(request: Request,db: Session = Depends(get_db)):
+  inf1 = crud.get_informe1(db)
+  return inf1
 
 @app.get("/challenge2/informe2")
-def informe2():
-  print("req2")
-  return True
+def informe2(request: Request,db: Session = Depends(get_db)):
+  inf2 = crud.get_informe2(db)
+  return inf2
+
+@app.get("/backup/h_empl")
+def backup(request: Request,db: Session = Depends(get_db)):
+  back = backup.do_backup1(db)
+  return back
+
+@app.get("/backup/job")
+def backup(request: Request,db: Session = Depends(get_db)):
+  back = backup.do_backup3(db)
+  return back
+
+@app.get("/backup/dep")
+def backup(request: Request,db: Session = Depends(get_db)):
+  back = backup.do_backup2(db)
+  return back
 
 @app.post("/batch/hiredemployees")
 async def b_hiredemployees():
